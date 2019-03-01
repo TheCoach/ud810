fb309bd94fb2119c68d26448643cf3defee11710
Yutian Yuan <yutyuan@gmail.com>
2019-03-01 15:54:58 +0100

    PS5 Add PS5

---
+++ ps5_python/lk.py
@@ -0,0 +1,129 @@
+import cv2 as cv
+import numpy as np
+import math
+
+def basic_lk(gray_img_1, gray_img_2, window_size = 5, t = 0.1):
+    if (gray_img_1.ndim != 2 or gray_img_2.ndim != 2):
+        raise Exception('gray_img_1 and gray_img_2 must be grayscale')
+
+    if (gray_img_1.shape != gray_img_2.shape):
+        raise Exception('gray_img_1 and gray_img_2 must be of the same shape')
+
+    if (window_size % 2 != 1):
+        raise Exception('window_size must be odd number')
+
+    half_w = int((window_size - 1) / 2)
+
+    gray_img_1 = np.pad(gray_img_1, half_w, 'constant')
+    gray_img_2 = np.pad(gray_img_2, half_w, 'constant')
+
+    ix = cv.Sobel(gray_img_1, cv.CV_64F, 1, 0)
+    iy = cv.Sobel(gray_img_1, cv.CV_64F, 0, 1)
+    it = np.float64(gray_img_2) - np.float64(gray_img_1)
+
+    ixx = ix * ix
+    ixy = ix * iy
+    iyy = iy * iy
+    ixt = ix * it
+    iyt = iy * it
+    ixx = cv.GaussianBlur(ixx, (window_size, window_size), 0)
+    ixy = cv.GaussianBlur(ixy, (window_size, window_size), 0)
+    iyy = cv.GaussianBlur(iyy, (window_size, window_size), 0)
+    ixt = cv.GaussianBlur(ixt, (window_size, window_size), 0)
+    iyt = cv.GaussianBlur(iyt, (window_size, window_size), 0)
+    
+    u = np.zeros_like(gray_img_1, dtype = np.float64)
+    v = np.zeros_like(gray_img_1, dtype = np.float64)
+
+    for r in range(half_w, gray_img_1.shape[0] - half_w):
+        for c in range(half_w, gray_img_2.shape[1] - half_w):
+            M = np.array([[ixx[r, c], ixy[r, c]], [ixy[r, c], iyy[r, c]]], dtype = np.float64)
+            w, _ = np.linalg.eig(M)
+            w = np.sort(w)[::-1]
+            if (np.linalg.matrix_rank(M) != 2 or w[0] / w[1] < t):
+                continue
+
+            b = np.array([[-ixt[r, c]], [-iyt[r, c]]], dtype = np.float64)
+            d = np.matmul(np.linalg.inv(M), b)
+            assert(d.shape == (2, 1))
+            u[r, c] = d[0, 0]
+            v[r, c] = d[1, 0]
+
+    u = u[half_w : u.shape[0] - half_w, half_w : u.shape[1] - half_w]
+    v = v[half_w : v.shape[0] - half_w, half_w : v.shape[1] - half_w]
+
+    return u, v
+
+def hi_lk(img_1, img_2, n):
+    if (math.floor(n) != n):
+        raise Exception('n must be an integer')
+
+    if (img_1.ndim != 2 or img_2.ndim != 2):
+        raise Exception('img_1 and img_2 must be grayscale')
+
+    if (img_1.shape != img_2.shape):
+        raise Exception('img_1 and img_2 must be of the same shape')
+
+    max_levels = math.floor(math.log(min(img_1.shape[0], img_1.shape[1])/2, 2))
+    if (n > max_levels):
+        print(f'n ({n}) is too big, overwrite to {max_levels}')
+
+    n = max_levels
+
+    g_img_1 = []
+    g_img_1.append(img_1)
+    g_img_2 = []
+    g_img_2.append(img_2)
+    for i in range(1, n + 1):
+        g_img_1.append(cv.pyrDown(g_img_1[i - 1]))
+        g_img_2.append(cv.pyrDown(g_img_2[i - 1]))
+
+    u = np.zeros_like(g_img_1[n], dtype = np.float64)
+    v = np.zeros_like(g_img_1[n], dtype = np.float64)
+
+    for k in range(n, -1, -1):
+        if k != n:
+            h, w = map(int, g_img_1[k].shape)
+            u = 2 * cv.pyrUp(u, dstsize = (w, h))
+            v = 2 * cv.pyrUp(v, dstsize = (w, h))
+        wk = warp(g_img_1[k], u, v)
+        dx, dy = basic_lk(wk, g_img_2[k])
+        u = u + dx
+        v = v + dy
+
+    return u, v;
+
+def draw_flow_grid(img, u, v, step = 16):
+    # scale displacements to be visible
+    u = 5 * u
+    v = 5 * v
+
+    h, w = img.shape[:2]
+    y, x = np.mgrid[step / 2 : h : step, step / 2 : w : step].reshape(2,-1).astype(int)
+    fx = u[y,x].T
+    fy = v[y,x].T
+    lines = np.vstack([x, y, x + fx, y + fy]).T.reshape(-1, 2, 2)
+    lines = np.int32(lines + 0.5)
+    vis = cv.cvtColor(img, cv.COLOR_GRAY2BGR)
+    cv.polylines(vis, lines, 0, (0, 255, 0))
+    for (x1, y1), (_x2, _y2) in lines:
+        cv.circle(vis, (x1, y1), 1, (0, 255, 0), -1)
+    return vis
+
+def draw_flow_hsv(u, v):
+    hsv = np.zeros((u.shape[0], u.shape[1], 3), dtype = np.uint8)
+    hsv[..., 1] = 255
+    mag, ang = cv.cartToPolar(u, v)
+    hsv[..., 0] = ang * 180 / np.pi / 2
+    hsv[..., 2] = cv.normalize(mag, None, 0, 255, cv.NORM_MINMAX)
+    bgr = cv.cvtColor(hsv, cv.COLOR_HSV2BGR)
+    return bgr
+
+def warp(img, u, v):
+    h, w = map(int, img.shape)
+    y, x = np.mgrid[0 : h : 1, 0 : w : 1].astype(np.float32)
+    warped = cv.remap(img, x + u.astype(np.float32), y + v.astype(np.float32), interpolation = cv.INTER_NEAREST)
+    warpedL = cv.remap(img, x + u.astype(np.float32), y + v.astype(np.float32), interpolation = cv.INTER_LINEAR)
+    I = warped == 0
+    warped[I] = warpedL[I]
+    return warped
---
+++ ps5_python/ps5.py
@@ -0,0 +1,224 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  1 13:23:26 2019
+
+@author: yutian.yuan
+"""
+
+import lk
+
+import sys
+import cv2 as cv
+import os
+import numpy as np
+from matplotlib import pyplot as plt
+
+def p1():
+    gray0 = cv.imread(os.path.join('input', 'TestSeq', 'Shift0.png'), cv.IMREAD_GRAYSCALE)
+    gray1 = cv.imread(os.path.join('input', 'TestSeq', 'ShiftR2.png'), cv.IMREAD_GRAYSCALE)
+    gray2 = cv.imread(os.path.join('input', 'TestSeq', 'ShiftR5U5.png'), cv.IMREAD_GRAYSCALE)
+    gray3 = cv.imread(os.path.join('input', 'TestSeq', 'ShiftR10.png'), cv.IMREAD_GRAYSCALE)
+    gray4 = cv.imread(os.path.join('input', 'TestSeq', 'ShiftR20.png'), cv.IMREAD_GRAYSCALE)
+    gray5 = cv.imread(os.path.join('input', 'TestSeq', 'ShiftR40.png'), cv.IMREAD_GRAYSCALE)
+
+    gray0 = cv.GaussianBlur(gray0, (11, 11), 0)
+    gray1 = cv.GaussianBlur(gray1, (11, 11), 0)
+    gray2 = cv.GaussianBlur(gray2, (11, 11), 0)
+    gray3 = cv.GaussianBlur(gray3, (11, 11), 0)
+    gray4 = cv.GaussianBlur(gray4, (11, 11), 0)
+
+    u, v = lk.basic_lk(gray0, gray1, 11)
+    vis = lk.draw_flow_grid(gray1, u, v)
+    cv.imwrite(os.path.join('output', 'ps5-1-a-1.png'), vis)
+
+    u, v = lk.basic_lk(gray0, gray2, 11)
+    vis = lk.draw_flow_grid(gray2, u, v)
+    cv.imwrite(os.path.join('output', 'ps5-1-a-2.png'), vis)
+
+    u, v = lk.basic_lk(gray0, gray3, 11)
+    vis = lk.draw_flow_grid(gray3, u, v)
+    cv.imwrite(os.path.join('output', 'ps5-1-b-1.png'), vis)
+
+    u, v = lk.basic_lk(gray0, gray4, 11)
+    vis = lk.draw_flow_grid(gray4, u, v)
+    cv.imwrite(os.path.join('output', 'ps5-1-b-2.png'), vis)
+
+    u, v = lk.basic_lk(gray0, gray5, 11)
+    vis = lk.draw_flow_grid(gray5, u, v)
+    cv.imwrite(os.path.join('output', 'ps5-1-b-3.png'), vis)
+
+def p2():
+    g = []
+    g.append(cv.imread(os.path.join('input', 'DataSeq1', 'yos_img_01.jpg'), cv.IMREAD_GRAYSCALE))
+    for i in range(1, 4):
+        g.append(cv.pyrDown(g[i - 1]))
+
+    plt.figure()
+    for i in range(0, 4):
+        plt.subplot(2, 2, i + 1)
+        plt.imshow(g[i], cmap='gray')
+        plt.title('G' + str(i))
+
+    plt.savefig(os.path.join('output', 'ps5-2-a-1.png'))
+
+    l = []
+    l.append(g[3])
+    for i in range(3, 0, -1):
+        h, w = map(int, g[i - 1].shape)
+        l.append(cv.subtract(g[i - 1], cv.pyrUp(g[i], dstsize=(w, h))))
+
+    l.reverse()
+    
+    plt.figure()
+    for i in range(0, 4):
+        plt.subplot(2, 2, i + 1)
+        plt.imshow(l[i], cmap='gray')
+    plt.savefig(os.path.join('output', 'ps5-2-b-1.png'))
+
+def p3_a():
+    g_img1 = []
+    g_img1.append(cv.imread(os.path.join('input', 'DataSeq1', 'yos_img_01.jpg'), cv.IMREAD_GRAYSCALE))
+    for i in range(1, 5):
+        g_img1.append(cv.pyrDown(g_img1[i - 1]))
+
+    g_img2 = []
+    g_img2.append(cv.imread(os.path.join('input', 'DataSeq1', 'yos_img_02.jpg'), cv.IMREAD_GRAYSCALE))
+    for i in range(1, 5):
+        g_img2.append(cv.pyrDown(g_img2[i - 1]))
+
+    g_img3 = []
+    g_img3.append(cv.imread(os.path.join('input', 'DataSeq1', 'yos_img_03.jpg'), cv.IMREAD_GRAYSCALE))
+    for i in range(1, 5):
+        g_img3.append(cv.pyrDown(g_img3[i - 1]))
+
+    G_LEVEL = 2
+
+    u, v = lk.basic_lk(g_img1[G_LEVEL], g_img2[G_LEVEL])
+    warpedI2 = lk.warp(g_img2[G_LEVEL], u, v)
+    vis12 = lk.draw_flow_hsv(u, v)
+
+    u, v = lk.basic_lk(g_img2[G_LEVEL], g_img3[G_LEVEL])
+    vis23 = lk.draw_flow_hsv(u, v)
+    vis = np.concatenate((vis12, vis23), axis=1);
+
+    diffImg = cv.subtract(g_img1[G_LEVEL], warpedI2)
+
+    cv.imwrite(os.path.join('output', 'ps5-3-a-1.png'), vis)
+    cv.imwrite(os.path.join('output', 'ps5-3-a-2.png'), diffImg)
+
+def p3_b():
+    g_img1 = []
+    g_img1.append(cv.imread(os.path.join('input', 'DataSeq2', '0.png'), cv.IMREAD_GRAYSCALE))
+    for i in range(1, 5):
+        g_img1.append(cv.pyrDown(g_img1[i - 1]))
+
+    g_img2 = []
+    g_img2.append(cv.imread(os.path.join('input', 'DataSeq2', '1.png'), cv.IMREAD_GRAYSCALE))
+    for i in range(1, 5):
+        g_img2.append(cv.pyrDown(g_img2[i - 1]))
+
+    g_img3 = []
+    g_img3.append(cv.imread(os.path.join('input', 'DataSeq2', '2.png'), cv.IMREAD_GRAYSCALE))
+    for i in range(1, 5):
+        g_img3.append(cv.pyrDown(g_img3[i - 1]))
+
+    G_LEVEL = 3
+
+    u, v = lk.basic_lk(g_img1[G_LEVEL], g_img2[G_LEVEL])
+    warpedI2 = lk.warp(g_img2[G_LEVEL], u, v)
+    vis12 = lk.draw_flow_hsv(u, v)
+
+    u, v = lk.basic_lk(g_img2[G_LEVEL], g_img3[G_LEVEL])
+    vis23 = lk.draw_flow_hsv(u, v)
+    vis = np.concatenate((vis12, vis23), axis=1);
+
+    diffImg = cv.subtract(g_img1[G_LEVEL], warpedI2)
+
+    cv.imwrite(os.path.join('output', 'ps5-3-b-1.png'), vis)
+    cv.imwrite(os.path.join('output', 'ps5-3-b-2.png'), diffImg)
+    
+def p4_a():
+    i1 = cv.imread(os.path.join('input', 'TestSeq', 'Shift0.png'), cv.IMREAD_GRAYSCALE)
+    i2 = cv.imread(os.path.join('input', 'TestSeq', 'ShiftR5U5.png'), cv.IMREAD_GRAYSCALE)
+    i3 = cv.imread(os.path.join('input', 'TestSeq', 'ShiftR10.png'), cv.IMREAD_GRAYSCALE)
+    i4 = cv.imread(os.path.join('input', 'TestSeq', 'ShiftR20.png'), cv.IMREAD_GRAYSCALE)
+    i5 = cv.imread(os.path.join('input', 'TestSeq', 'ShiftR40.png'), cv.IMREAD_GRAYSCALE)
+
+    u3,v3 = lk.hi_lk(i1, i3, 10)
+    u4,v4 = lk.hi_lk(i1, i4, 10)
+    u5,v5 = lk.hi_lk(i1, i5, 10)
+
+    flow1 = lk.draw_flow_hsv(u3, v3)
+    flow2 = lk.draw_flow_hsv(u4, v4)
+    flow3 = lk.draw_flow_hsv(u5, v5)
+    
+    warped_i3 = lk.warp(i3, u3, v3)
+    warped_i4 = lk.warp(i4, u4, v4)
+    warped_i5 = lk.warp(i5, u5, v5)
+
+    plt.figure()
+    plt.subplot(1, 3, 1)
+    plt.axis("off")
+    plt.imshow(cv.cvtColor(flow1, cv.COLOR_BGR2RGB))
+    plt.subplot(1, 3, 2)
+    plt.imshow(cv.cvtColor(flow2, cv.COLOR_BGR2RGB))
+    plt.subplot(1, 3, 3)
+    plt.imshow(cv.cvtColor(flow3, cv.COLOR_BGR2RGB))
+
+    plt.savefig(os.path.join('output', 'ps5-4-a-1.png'))
+
+    plt.figure()
+    plt.axis("off")
+    plt.subplot(1, 3, 1)
+    plt.imshow(cv.subtract(i1, warped_i3), cmap='gray')
+    plt.subplot(1, 3, 2)
+    plt.imshow(cv.subtract(i1, warped_i4), cmap='gray')
+    plt.subplot(1, 3, 3)
+    plt.imshow(cv.subtract(i1, warped_i5), cmap='gray')
+
+    plt.savefig(os.path.join('output', 'ps5-4-a-2.png'))
+    
+    u2, v2 = lk.hi_lk(i1, i2, 10)
+    flow4 = lk.draw_flow_grid(i2, u2, v2)
+    cv.imwrite(os.path.join('output', 'ps5-4-a-3.png'), flow4)
+
+def p4_b():
+    i1 = cv.imread(os.path.join('input', 'DataSeq1', 'yos_img_01.jpg'), cv.IMREAD_GRAYSCALE)
+    i2 = cv.imread(os.path.join('input', 'DataSeq1', 'yos_img_02.jpg'), cv.IMREAD_GRAYSCALE)
+    i3 = cv.imread(os.path.join('input', 'DataSeq1', 'yos_img_03.jpg'), cv.IMREAD_GRAYSCALE)
+
+    u2,v2 = lk.hi_lk(i1, i2, 10)
+    u3,v3 = lk.hi_lk(i2, i3, 10)
+    flow2 = lk.draw_flow_hsv(u2, v2)
+    flow3 = lk.draw_flow_hsv(u3, v3)
+    
+    warped_i2 = lk.warp(i2, u2, v2)
+    warped_i3 = lk.warp(i3, u3, v3)
+
+    cv.imwrite(os.path.join('output', 'ps5-4-b-1.png'), flow2)
+    cv.imwrite(os.path.join('output', 'ps5-4-b-2.png'), flow3)
+    cv.imwrite(os.path.join('output', 'ps5-4-b-3.png'), cv.subtract(i1, warped_i2))
+    cv.imwrite(os.path.join('output', 'ps5-4-b-4.png'), cv.subtract(i2, warped_i3))
+
+def p4_c():
+    i1 = cv.GaussianBlur(cv.imread(os.path.join('input', 'DataSeq2', '0.png'), cv.IMREAD_GRAYSCALE), (5, 5), 0)
+    i2 = cv.GaussianBlur(cv.imread(os.path.join('input', 'DataSeq2', '1.png'), cv.IMREAD_GRAYSCALE), (5, 5), 0)
+    i3 = cv.GaussianBlur(cv.imread(os.path.join('input', 'DataSeq2', '2.png'), cv.IMREAD_GRAYSCALE), (5, 5), 0)
+
+    u2,v2 = lk.hi_lk(i1, i2, 10)
+    flow2 = lk.draw_flow_hsv(u2, v2)
+    u3,v3 = lk.hi_lk(i2, i3, 10)
+    flow3 = lk.draw_flow_hsv(u3, v3)
+    warped_i2 = lk.warp(i2, u2, v2)
+    warped_i3 = lk.warp(i3, u3, v3)
+    cv.imwrite(os.path.join('output', 'ps5-4-c-1.png'), flow2)
+    cv.imwrite(os.path.join('output', 'ps5-4-c-2.png'), flow3)
+    cv.imwrite(os.path.join('output', 'ps5-4-c-3.png'), cv.subtract(i1, warped_i2))
+    cv.imwrite(os.path.join('output', 'ps5-4-c-4.png'), cv.subtract(i2, warped_i3))
+
+def main(argv):
+    p3_a()
+    p3_b()
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
